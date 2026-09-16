7083be82e711a354a5af843303084790f45675bf
Shivajith J <shivajithj@Shivajiths-Mac-mini.local>
2026-09-16 12:01:32 +0530

    Initial iOS demo app

---
+++ demo-ios/demo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_iosApp.swift
+//  demo-ios
+//
+//  Created by Shivajith J on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct demo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
